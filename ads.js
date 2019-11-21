239714a155d7ee3e87c84769c5bb65a80f2d8eb7
downloadgila <56267929+downloadgila@users.noreply.github.com>
2019-11-21 07:27:01 +0700

    Create ads.js

---
+++ ads.js
@@ -0,0 +1,68 @@
+<div style="background-color:transparent; padding:10px 5px 11px 5px; border:1px solid #ddd; margin-bottom:-40px; width:98%; margin-bottom:10px;"><script language="JavaScript">
+<!-- Begin
+var jumlah_iklan = 7; / Jumlah Iklan /
+var waktu_sekarang = new Date()
+var detik = waktu_sekarang.getSeconds()
+var pasang_iklan = detik % jumlah_iklan;
+pasang_iklan +=1;
+if (pasang_iklan==1) {
+txt="Terima hingga 50 token CPU gratis dengan bergabung dengan Airdrip. CPUcoin dapat mengumpulkan sumber daya cloud untuk digunakan dan merestrukturisasi menjadi jaringan sumber daya komputasi yang sangat luas hingga bisa menghasilkan mata uang Crypto Currencies.";
+url="http://bit.ly/qoinprominer";
+title="Cara Mudah Menghasilkan Mata Uang Crypto Dengan Qoinpro";
+judul="Cara Mudah Menghasilkan Mata Uang Crypto Dengan Qoinpro";
+link="www.qoinpro.com";
+}
+if (pasang_iklan==2) {
+txt="The best browser to watch videos on YouTube or browse anything on the internet so that you can generate payments in Bitcoins. Download the browser here...";
+url="http://bit.ly/browserearnbtc";
+title="Download browser for Youtube and earn more Bitcoin";
+judul="Download browser for Youtube and earn more Bitcoin";
+link="www.BrowserCrypto.com";
+}
+if (pasang_iklan==3) {
+txt="Free Download Mp3 Top Chart, Top Hts, Pop, Rock, Folk, Ballads, Reggae, Punk, Metal, Dangdut, etc....";
+url="http://downloaddgila.blogspot.com";
+title="Free Download Mp3";
+judul="Free Download Mp3";
+link="www.downloaddgila.com";
+}
+if (pasang_iklan==4) {
+txt="Free Download Mp3, Lyrics, Video here....";
+url="http://lyricsexplorer.blogspot.com";
+title="Download Lyrics Top Chart Mp3";
+judul="Download Lyrics Top Chart Mp3";
+link="www.lyricsexplorer.com";
+}
+if (pasang_iklan==5) {
+txt="Sharing all about Blogging, Tutorial, Cryptocurrency, Wallet Coin, mining Bitcoin, Ethereum, Dogecoin, Litecoin, Auto Faucet, and online business....";
+url="https://minerminds.blogspot.com/";
+title="Minerminds";
+judul="Blogging for share about Cryptocurrency";
+link="www.minerminds.com";
+}
+if (pasang_iklan==6) {
+txt="Win upto $200 in Bitcoins every hour, no strings attached! Multiply your bitcoins, free weekly lottery with big prizes, 50% referral commissions and much more!";
+url="http://bit.ly/freebitcoinlegit";
+title="Free Bitcoin Mining Sources";
+judul="Free Bitcoin Mining Sources";
+link="www.freebitco.in";
+}
+if (pasang_iklan==7) {
+txt="Free download wallpaper windows xp, windows vista, windows 7, mac OS, nature wallpapers, celebrities wallpapers, etc.......";
+url="http://wallpapers-area.blogspot.com";
+title="Free Download Wallpapers";
+judul="Free Download Wallpapers";
+link="www.wallpapers-area.com";
+}
+document.write('<div style="padding:0px 10px 0px 10px;">');
+document.write('<a href=\"' + url + '\"_top\"');
+document.write('title=\"' + title + '\" target="_blank">');
+document.write('<span style="font-size:20px; font-weight:bold; color:#000;">' + judul + '</span></a><br>');
+document.write('<normal>' + txt + '</normal><br>');
+document.write('<a href=\"' + url + '\"_top\"');
+document.write('title=\"' + title + '\" target="_blank">');
+document.write('<span style="color:#333;"><small>' + link + '</small></span></a>');
+document.write('</div>');
+// End -->
+</script>
+<div style="background: repeating-linear-gradient(135deg,transparent 0,transparent 20%,#ddd 100px,#fff 1000px); width:100px; height:20px; padding-right:0px; float:right; margin-top:-8px; margin-right:-5px;"><span style="float:right; padding-right:5px; margin-bottom:6px;"><a href="https://informasikontes.blogspot.com/" target="_blank"><span style="color:#777; font-size:9px; text-align:right;">Ads News</span></a></span></div></div>
